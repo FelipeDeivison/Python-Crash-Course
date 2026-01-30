f952aa80dacc5d575572a4e6cf2dd40bc350609c
Felipe Deivison <felipedeivison@outlook.com.br>
2026-01-30 18:28:51 -0300

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,26 @@
+#Numeros e calculos simples
+adicao = (5 +  3)
+print(adicao)
+
+subtracao = (13 - 5)
+print(subtracao)
+
+multiplicacao = (4 * 2)
+print(multiplicacao)
+
+divisao = (16 / 2)
+print(divisao)
+
+number_favorite = 8
+print(f'Meu número favorito é o {number_favorite}!')
+
+#Underscode em numeros
+Underscode_number = 8_000_000_000
+print(Underscode_number)
+
+#Constantes, numero que se permanece o mesmo durante todo o programa.
+
+NUMBER_FAV = 8 #usamos as variavel com todas as letras maiusculas.
+print(NUMBER_FAV)
+
+import this #Zen do python
