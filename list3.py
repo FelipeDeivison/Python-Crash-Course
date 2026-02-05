3154678fd647c690132c63783d463983f14a9bda
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-05 18:29:56 -0300

    Add files via upload

---
+++ list3.py
@@ -0,0 +1,12 @@
+pizzas = ['Frango com Catupiry', 'Calabresa', 'Carne Seca']
+
+for pizza in pizzas:
+    print(f'Gosta de pizza de {pizza}')
+print("\n\n\n")
+print('Gosto muito de pizza, porém só posso comer uma vez por semana.')
+print("\nEu amo pizza!\n")
+
+animais = ['Lobo', 'Cachorro', 'Leão']
+for animal in animais:
+    print(f'Gosto de como o {animal} costuma caçar!')
+print("\nQualquer um desses animais é um ótimo caçador!")
