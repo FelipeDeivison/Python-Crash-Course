2cf0bfa7c2776160be3c3c93e734b83b67148634
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-07 14:42:05 -0300

    Atualizei seguindo as normas da PEP8

--- name_cases.py
+++ name_cases.py
@@ -6,11 +6,21 @@ print(pessoa.upper())
 print(pessoa.title())
 
 autor = 'Steve Jobs'
-citacao = '“Seu trabalho vai preencher boa parte da sua vida, e a única maneira de estar realmente satisfeito é fazer o que você acredita ser um ótimo trabalho. E a única maneira de fazer um ótimo trabalho é amar o que você faz.”'
+citacao = (
+    '“Seu trabalho vai preencher boa parte da sua vida, e a única maneira de '
+    'estar realmente satisfeito é fazer o que você acredita ser um ótimo '
+    'trabalho. E a única maneira de fazer um ótimo trabalho é amar '
+    'o que você faz.”'
+    )
 print(f'{autor} disse uma vez: {citacao}')
 
 famous_person = ' Steve Jobs '
-message = ' “Seu trabalho vai preencher boa parte da sua vida, e a única maneira de estar realmente satisfeito é fazer o que você acredita ser um ótimo trabalho. E a única maneira de fazer um ótimo trabalho é amar o que você faz.” '
+message = (
+    '“Seu trabalho vai preencher boa parte da sua vida, e a única maneira de '
+    'estar realmente satisfeito é fazer o que você acredita ser um ótimo '
+    'trabalho. E a única maneira de fazer um ótimo trabalho é amar '
+    'o que você faz.”'
+    )
 
 print(f'{famous_person.lstrip().rstrip()} disse uma vez:\n \t{message.strip()}')
 
