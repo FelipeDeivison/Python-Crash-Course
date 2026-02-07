9d4a42331bd1589a6de39088748abf1ef8421d22
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-07 14:01:56 -0300

    Add files via upload

--- tuplas.py
+++ tuplas.py
@@ -1,12 +1,14 @@
-buffets = ('Café da manhã', 'Almoço balanceado', 'Lanchinho da tarde', 'Jantar leve', 'Ceia leve')
+buffets = ('Café da manhã', 'Almoço balanceado', 'Lanchinho da tarde',
+            'Jantar leve', 'Ceia leve')
 for buffet in buffets:
     print(buffet)
-#Tuplas não podem ser modificadas, segue exemplo:
+# Tuplas não podem ser modificadas, segue exemplo:
 
-#buffets[3] =  ('jantar')
-#print(buffets)
+# buffets[3] =  ('jantar')
+# print(buffets)
 
-#Porém elas podem ser substituidas
-buffets = ('Café da manhã', 'Almoço balanceado', 'Lanchinho da tarde', 'Jantar', 'Merenda da noite')
+# Porém elas podem ser substituidas
+buffets = ('Café da manhã', 'Almoço balanceado', 'Lanchinho da tarde', 'Jantar',
+            'Merenda da noite')
 for buffet in buffets:
     print(buffet)
