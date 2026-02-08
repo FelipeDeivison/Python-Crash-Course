1c554f1d0c4cdffee25cc967b7055731d6468238
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-08 14:27:13 -0300

    Primeira versão do projeto "cara ou coroa"

---
+++ cara_ou_coroa.py
@@ -0,0 +1,16 @@
+# Cara ou Coroa!
+# Projeto Pessoal, irei aprimorar com o tempo.
+# Regras:
+# Se as duas moedas derem 'cara' o ponto vai para o João, mas se as moedas
+# derem 'coroa' o valor vai para a Ana. Caso o resultado for diferentes o ponto
+# fica com a casa.
+
+joão = 'coroa'
+ana = 'cara'
+
+if (joão == 'coroa') and (ana == 'coroa'):
+    print('Um ponto vai para Ana')
+elif (joão == 'cara') and (ana == 'cara'):
+    print('O ponto vai para o João')
+else:
+    print('O ponto vai para a casa')
