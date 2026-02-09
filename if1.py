d0fe20eea7cf31cf34b187411916034f5c7948ea
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-09 17:55:54 -0300

    Add files via upload

---
+++ if1.py
@@ -0,0 +1,60 @@
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('Jogador ganhau 5 pontos!')
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('Jogador ganhau 5 pontos!')
+
+alien_color = 'red'
+if alien_color == 'green':
+    print('Jogador ganhou 5 pontos por abrir fogo contra o alienígena')
+elif alien_color == 'red':
+    print('Jogador ganhou 15 pontos')
+else:
+    print('Jogador ganhou 10 pontos!')
+
+alien_color = 'green'
+if alien_color == 'green':
+    print('Jogador ganhou 5 pontos por abrir fogo contra o alienígena')
+elif alien_color == 'red':
+    print('Jogador ganhou 15 pontos')
+else:
+    print('Jogador ganhou 10 pontos!')
+    
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print('Jogador ganhou 5 pontos por abrir fogo contra o alienígena')
+elif alien_color == 'red':
+    print('Jogador ganhou 15 pontos')
+else:
+    print('Jogador ganhou 10 pontos!')
+
+age = 23
+
+if age < 2:
+    print('Você é um neném!')
+elif age < 4:
+    print('Você é uma criança!')
+elif age < 13:
+    print('Você é um garoto(a)!')
+elif age < 20:
+    print('Você é um adolescente!')
+elif age < 65:
+    print('Você é um adulto!')
+else:
+    print('Você é um idoso!')
+
+favorite_fruits = ['Morango', 'Melancia', 'Uva', 'Melão', 'Banana']
+
+if 'Uva' in favorite_fruits:
+    print('Você gosta mesmo de Uva!')
+if 'Morango' in favorite_fruits:
+    print('Você gosta mesmo de Morango!')
+if 'Melancia' in favorite_fruits:
+    print('Você gosta mesmo de Melancia!')
+if 'Melão' in favorite_fruits:
+    print('Você gosta mesmo de Melão!')
+if 'banana'.title() in favorite_fruits:
+    print('Você gosta mesmo de Banana!')
