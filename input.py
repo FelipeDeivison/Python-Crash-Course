db792700ef369ea9ccdfa69f806b9de04df3bbb7
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-19 23:09:54 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,22 @@
+# carro = input('Qual o modelo do veiculo deseja alugar? ')
+# print(f'Vejamos se consigo encontrar o modelo {carro} para você!\n')
+
+# print(
+#     'Boa noite!'
+#     '\nSejam Bem vindo ao Restaurante.'
+#     )
+# reserva = int(input('Quantos lugares deseja reservar? '))
+# if reserva > 8:
+#     print(
+#         'No momento não temos uma mesa disponivel\n'
+#         'Vocês podem esperar que logo mais estará disponivel.'
+#         )
+# else:
+#     print('Temos uma mesa á disposiçâo.')
+
+number = int(input('Digite um número: '))
+
+if number % 10 == 0:
+    print(f'O número "{number}" é miltiplo de 10!')
+else:
+    print(f'O númoro "{number}" não é multiplo de 10.')
