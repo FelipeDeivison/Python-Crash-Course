ae836224a4fe4a9434a547dd372cc0210621c9bf
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-19 23:11:45 -0300

    Add files via upload

--- input.py
+++ input.py
@@ -1,19 +1,24 @@
-# carro = input('Qual o modelo do veiculo deseja alugar? ')
-# print(f'Vejamos se consigo encontrar o modelo {carro} para você!\n')
+#Exercicio 1
 
-# print(
-#     'Boa noite!'
-#     '\nSejam Bem vindo ao Restaurante.'
-#     )
-# reserva = int(input('Quantos lugares deseja reservar? '))
-# if reserva > 8:
-#     print(
-#         'No momento não temos uma mesa disponivel\n'
-#         'Vocês podem esperar que logo mais estará disponivel.'
-#         )
-# else:
-#     print('Temos uma mesa á disposiçâo.')
+carro = input('Qual o modelo do veiculo deseja alugar? ')
+print(f'Vejamos se consigo encontrar o modelo {carro} para você!\n')
 
+#Exercicio 2
+
+print(
+    'Boa noite!'
+    '\nSejam Bem vindo ao Restaurante.'
+    )
+reserva = int(input('Quantos lugares deseja reservar? '))
+if reserva > 8:
+    print(
+        'No momento não temos uma mesa disponivel\n'
+        'Vocês podem esperar que logo mais estará disponivel.'
+        )
+else:
+    print('Temos uma mesa á disposiçâo.')
+
+#Exercicio 3
 number = int(input('Digite um número: '))
 
 if number % 10 == 0:
