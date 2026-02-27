2210459bbce02203b3c9e9469dfee5ccb5b8b8f5
Felipe Deivison <felipedeivison@outlook.com.br>
2026-02-26 23:58:08 -0300

    Add files via upload

---
+++ funcoes3.py
@@ -0,0 +1,9 @@
+def city_country(cidade, pais):
+    print(f'"{cidade}, {pais}"')
+
+city_country('santiago'.title(), 'chile'.title())
+
+city_country(pais= 'Brasil', cidade= 'Rio de Janeiro')
+
+city_country('Medellín', 'Colombia')
+
