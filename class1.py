f900d4b11c010eaa06270eea215a9694ca497492
Felipe Deivison <felipedeivison@outlook.com.br>
2026-03-17 18:20:44 -0300

    Add files via upload

--- class1.py
+++ class1.py
@@ -1,9 +1,11 @@
 class User:
-    def __init__(self, first_name, last_name, age, location,):
+    def __init__(self, first_name, last_name, age, location, login_attempts=0,):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
         self.location = location
+        self.login_attempts = login_attempts
+    
     def describe_user(self):
         print(
             f'Informações do Usuário:\n'
@@ -13,12 +15,30 @@ class User:
             )
 
     def greet_usr(self):
-        print(f'Olá {self.first_name} {self.last_name}, seja Bem-Vindo!')
+        print(f'Olá {self.first_name} {self.last_name}, seja Bem-Vindo!\n')
+
+    def increment_login_attempts(self,):
+        self.login_attempts += 1
+        print(self.login_attempts)
+    
+    def reset_login_attempts(self,):
+        self.login_attempts = 0
+        print(self.login_attempts)
+        
 
     
-user = User('Felipe', 'Angelo', '23', 'São Paulo')
+user = User('Felipe', 'Angelo', 23, 'São Paulo', login_attempts=25)
 
 user.describe_user()
 user.greet_usr()
+
+user2 = User('Natiara', 'Angelo', 32, 'Bela Cruz')
+user2.describe_user()
+user2.greet_usr()
     
-        
+
+user.increment_login_attempts()
+
+user.reset_login_attempts()
+
+print(user.login_attempts)
