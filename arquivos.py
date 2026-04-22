b811aa79a32ce51d9e81f09de8348160a86b7ce5
Felipe Deivison <felipedeivison@outlook.com.br>
2026-04-21 21:22:19 -0300

    Add files via upload

--- arquivos.py
+++ arquivos.py
@@ -3,3 +3,13 @@ from pathlib import Path
 txt = Path('C:/Users/Felip/OneDrive/Área de Trabalho/python_word/text_files/learning_python.txt')
 mensagem = txt.read_text()
 print(mensagem)
+
+mensagens = mensagem.splitlines()
+texto = []
+texto.append(mensagens)
+
+mensagens = '\n'.join(mensagens)
+
+mensagens = mensagens.replace('python', 'linguagem C')
+
+print(mensagens)
